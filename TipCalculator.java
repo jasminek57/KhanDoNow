972c0a6dead36125d901ac5ac4e96502c724c505
bt_1n3_24 <sample@bths.edu>
2023-09-21 08:35:05 -0400

    introduction statements added

---
+++ TipCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Welcome to Giggle Resturaunt!");
+        System.out.print("How many people are eating today? ");
+        int peopleNum = scan.nextInt();
+
+//        while (number % 2 != 1) {
+//            System.out.print("No silly, i said ODD not even! Try again: ");
+//            number = scan.nextInt();
+//        }
+//
+//        System.out.print("Done!");
+    }
+}
+
+
