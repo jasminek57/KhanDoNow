3deab475a862b4d275ec7aa3928bb93e0f6aeb65
bt_1n3_24 <sample@bths.edu>
2023-09-21 08:49:37 -0400

    variables and print statements before tip added

--- TipCalculator.java
+++ TipCalculator.java
@@ -7,15 +7,21 @@ public class TipCalculator {
         int people = scan.nextInt();
         System.out.print("Enter your tip percentage as an integer: ");
         int percent = scan.nextInt();
+        System.out.print("Your tip percentage will be %" + percent);
         double totalPrice = 0;
 
         while (itemv != -1) {
-            System.out.print("Enter the cost of the item you order (enter -1 to stop): ");
+            System.out.print("Enter the cost of the item(s) you order (enter -1 to stop): ");
             double price = scan.nextDouble();
             totalPrice += price;
         }
 
+        double perPerson = totalPrice / people;
+        System.out.print("Price before tip: $" + totalPrice);
+        System.out.print("Price per person before tip: $" + perPerson);
+
         double tip = (percent * totalPrice) / 100;
+        System.out.print("Tip amount being added to bill: $" + tip);
         double singularTip = tip / people;
 
     }
